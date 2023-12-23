fa1d4e2b9c908fe7580ef5c390800e14e8f36263
Sayon Dutta <52020690+Sayon2000@users.noreply.github.com>
2023-12-24 01:58:43 +0530

    getting and sending messages from frontend

--- backend/controllers/messageControllers.js
+++ backend/controllers/messageControllers.js
@@ -1,13 +1,27 @@
+const { fn , Sequelize , col,Op } = require('sequelize');
 const Message = require('../models/Message')
+const User = require('../models/User')
 
-exports.addMessage = async(req,res)=>{
-    try{
-        const message  =req.body.message;
-        const result = await req.user.createMessage({message : message})
-        return res.json({success : true , message : result})
-    }catch(e){
+exports.addMessage = async (req, res) => {
+    try {
+        const message = req.body.message;
+        const result = await req.user.createMessage({ message: message })
+        return res.json({ success: true, message: result })
+    } catch (e) {
         console.log(e)
-        return res.status(500).json({success : false , msg : "Internal server error"})
+        return res.status(500).json({ success: false, msg: "Internal server error" })
     }
 
+}
+
+exports.getMessages = async (req, res) => {
+    try {
+        const result = await Message.findAll();
+        
+        return res.json({success : true , messages : result , id : req.user.id})
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({ success: false, msg: "Internal server error" })
+
+    }
 }
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -6,15 +6,28 @@ async function renderElemets(){
         if(!localStorage.getItem('token')){
             window.location = 'login.html'
         }
-const res = await axios.get('http://localhost:4000/user/all-users' , {
+const p1 =  axios.get('http://localhost:4000/user/all-users' , {
     headers : {
         'auth-token' : localStorage.getItem('token')
     }
     
 })
+const p2 =  axios.get('http://localhost:4000/message/get-messages' , {
+    headers : {
+        'auth-token' : localStorage.getItem('token')
+    }
+    
+})
+
+const [res , messages ] = await Promise.all([p1,p2])
 console.log(res)
+console.log(messages)
 res.data.users.forEach(user => {
     showUser(user)
+})
+const id = messages.data.id
+messages.data.messages.forEach(message => {
+    showMessage(message , id === message.userId)
 })
     }catch(e){
         console.log(e)
@@ -26,4 +39,36 @@ function showUser(user){
     div.textContent = user.name +' joined'
     div.className = 'o-message'
     messages.appendChild(div)
+}
+
+function showMessage(data , user){
+    const div = document.createElement('div')
+    div.textContent = data.message
+    if(user){
+        div.className = 'u-message'
+    }else{
+        div.className = 'o-message'
+
+    }
+
+    messages.appendChild(div)
+}
+
+document.forms[0].addEventListener('submit' , sendMessage)
+
+async function sendMessage(e){
+    try{
+        e.preventDefault()
+        const data = {message : e.target.message.value}
+        const res = await axios.post('http://localhost:4000/message/add-message' , data , {
+            headers : {
+                'auth-token' : localStorage.getItem('token')
+            }
+        })
+        console.log(res)
+        showMessage(data , true)
+        e.target.message.value =''
+    }catch(e){
+        console.log(e)
+    }
 }
